1e866616e4b3dca035cc4e8301dbe4e9c0500160
AnushaHiremath <hiremath.anusha@gmail.com>
2021-04-23 12:26:25 +0530

    updated task-1

--- task1.js
+++ task1.js
@@ -5,3 +5,22 @@
 // Output: 1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 FooBar
 
 // Solutions
+function FooBar(n) {
+    n = 15;
+    for (let i = 1; i <= n; i++) {
+        // console.log(i)
+        if (i % 3==0) {
+            console.log('foo')
+        }
+        else if (i % 5 == 0) {
+            console.log('bar')
+        }
+        else if (i % 3 == 0 || i % 5 == 0) {
+            console.log('foobar')
+        }
+        else{
+            console.log(i)
+        }
+    }
+}
+console.log(FooBar())
